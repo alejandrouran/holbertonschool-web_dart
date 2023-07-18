fe9512beb2b70abe48dc4e0edeb103fb61e74cb9
Alejandro Uran <alejandrourn@gmail.com>
2023-07-18 14:45:39 -0500

    two task function dart

---
+++ 0x01-dart-function/2-sumfunc.dart
@@ -0,0 +1,12 @@
+int add(int a, int b) {
+  return a + b; 
+}
+
+int sub(int a, int b) {
+  return a - b;
+}
+
+String showFunc(int a, int b) {
+  print("Add $a + $b = ${add(a, b)}");
+  return("Sub $a - $b = ${sub(a, b)}");
+}

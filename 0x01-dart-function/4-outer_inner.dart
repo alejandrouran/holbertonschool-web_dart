39fa03d95b61bb78f639abed3a6db6460b1cd0a7
Alejandro Uran <alejandrourn@gmail.com>
2023-07-18 15:52:23 -0500

    four task function dart

---
+++ 0x01-dart-function/4-outer_inner.dart
@@ -0,0 +1,7 @@
+void outer(String name, String id){
+  String inner(){
+    var splitter = name.split(" ");
+    var alias = splitter[1].substring(0, 1) + "." + splitter[0];
+    return "Hello Agent ${alias} your id is $id";
+  }
+}

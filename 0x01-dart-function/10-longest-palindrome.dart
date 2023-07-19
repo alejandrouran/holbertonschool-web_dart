d3621413af1fc5ebc42f7470c620ab2b1511c0a0
Alejandro Uran <alejandrourn@gmail.com>
2023-07-19 10:32:30 -0500

    ten task function dart

--- 0x01-dart-function/10-longest-palindrome.dart
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -1,36 +1,39 @@
-String longestPalindrome(String s) {
-  if (s.length < 3) {
-    return null;
-  }
-
-  String longestPalindrome = '';
-  int maxLength = 0;
-
-  for (int i = 0; i < s.length; i++) {
-    for (int j = i + 1; j <= s.length; j++) {
-      String substring = s.substring(i, j);
-      if (isPalindrome(substring) && substring.length > maxLength) {
-        maxLength = substring.length;
-        longestPalindrome = substring;
-      }
-    }
-  }
-
-  return longestPalindrome.isNotEmpty ? longestPalindrome : null;
+bool isAlphaNumeric(String s) {
+  return s.codeUnits.every((int c) => c >= 'a'.codeUnitAt(0) && c <= 'z'.codeUnitAt(0) || c >= 'A'.codeUnitAt(0) && c <= 'Z'.codeUnitAt(0) || c >= '0'.codeUnitAt(0) && c <= '9'.codeUnitAt(0));
 }
 
 bool isPalindrome(String s) {
-  int left = 0;
-  int right = s.length - 1;
-
-  while (left < right) {
-    if (s[left] != s[right]) {
+  var i = 0;
+  var j = s.length - 1;
+  if (s.length < 3) {
+    return false;
+  }
+  while (i < j) {
+    while (i < j && !isAlphaNumeric(s[i])) {
+      i++;
+    }
+    while (i < j && !isAlphaNumeric(s[j])) {
+      j--;
+    }
+    if (s[i].toLowerCase() != s[j].toLowerCase()) {
       return false;
     }
-
-    left++;
-    right--;
+    i++;
+    j--;
   }
-
   return true;
+}
+String longestPalindrome(String s) {
+  if (s.length < 3) return 'none';
+  String longest = '';
+  String current = '';
+  for (int i = 0; i < s.length; i++) {
+    for (int j = i; j < s.length; j++) {
+      current = s.substring(i, j + 1);
+      if (isPalindrome(current) && current.length > longest.length) {
+        longest = current;
+      }
+    }
+  }
+  return longest != '' ? longest : 'none';
 }

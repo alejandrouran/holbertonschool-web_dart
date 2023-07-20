ae928b9d7d37982b3d0b060d7351ded02c0d5648
Alejandro Uran <alejandrourn@gmail.com>
2023-07-20 14:38:52 -0500

    five task classes dart

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -1,19 +1,10 @@
 class User {
-  int id;
-  String name;
+  int id = 0;
+  String name = "";
   int age;
   double height;
 
   User({required this.id, required this.name, required this.age, required this.height});
-
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'name': name,
-      'age': age,
-      'height': height,
-    };
-  }
 
   static User fromJson(Map<dynamic, dynamic> userJson) {
     return User(
@@ -24,8 +15,17 @@ class User {
     );
   }
 
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height,
+    };
+  }
+
   @override
   String toString() {
-    return "User(id: $id, name: $name, age: $age, height: $height)";
+    return 'User(id : $id ,name: $name,  age: $age, height: $height)';
   }
 }

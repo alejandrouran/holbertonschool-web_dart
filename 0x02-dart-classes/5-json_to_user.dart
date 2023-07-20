1e19f1c348f5122f2502b424cbb7c339d1235c95
Alejandro Uran <alejandrourn@gmail.com>
2023-07-20 14:36:23 -0500

    five task classes dart

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -29,14 +29,3 @@ class User {
     return "User(id: $id, name: $name, age: $age, height: $height)";
   }
 }
-
-void main() {
-  final djo = User(id: 1, name: "Djo", age: 25, height: 1.89);
-  print(djo.toJson());
-
-  Map map = {'id': 3, 'name': 'Youssef', 'age': 26, 'height': 1.9};
-  final youssef = User.fromJson(map);
-
-  print(djo.toString());
-  print(youssef.toString());
-}

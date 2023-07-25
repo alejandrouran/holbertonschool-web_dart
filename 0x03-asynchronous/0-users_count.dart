397a80d27b77318bf1901b51af637c1f196b8f87
Alejandro Uran <alejandrourn@gmail.com>
2023-07-25 08:54:07 -0500

    update 0 task asynchronous

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -1,18 +1,8 @@
-import 'dart:async';
-
 Future<int> fetchUsersCount() => Future.delayed(
       const Duration(seconds: 2),
       () => 19,
     );
 
-Future<void> usersCount() async {
-  // Call the fetchUsersCount() function to get the user count.
-  int userCount = await fetchUsersCount();
-
-  // Print the number of users.
-  print(userCount);
-}
-
-void main() async {
-  await usersCount();
-}
+  userCount() async {
+    print(await fetchUsersCount());
+  }

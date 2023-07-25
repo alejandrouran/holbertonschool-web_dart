de8a811fe00a78761ab9039a40c9320a8c8eb101
Alejandro Uran <alejandrourn@gmail.com>
2023-07-25 06:27:03 -0500

    tasks zero and one dart

---
+++ 0x03-asynchronous/0-users_count.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+
+Future<int> fetchUsersCount() => Future.delayed(
+      const Duration(seconds: 2),
+      () => 19,
+    );
+
+Future<void> usersCount() async {
+  // Call the fetchUsersCount() function to get the user count.
+  int userCount = await fetchUsersCount();
+
+  // Print the number of users.
+  print(userCount);
+}
---
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+import 'dart:convert';
+
+Future<String> fetchUserData() => Future.delayed(
+      const Duration(seconds: 2),
+      () =>
+          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
+    );
+
+Future<String> getUserId() async {
+  // Call the fetchUserData() function to get user data.
+  String userDataJson = await fetchUserData();
+
+  // Parse the JSON data to a Map.
+  Map<String, dynamic> userData = json.decode(userDataJson);
+
+  // Extract and return the user's ID.
+  String userId = userData['id'];
+  return userId;
+}

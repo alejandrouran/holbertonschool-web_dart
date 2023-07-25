8ad4ab05e1c224a1f9911d3688d80a0615c203f3
Alejandro Uran <alejandrourn@gmail.com>
2023-07-25 09:09:47 -0500

    update one task asynchronous

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -2,12 +2,6 @@ import 'dart:async';
 import 'dart:convert';
 import '1-util.dart';
 
-Future<String> fetchUserData() => Future.delayed(
-      const Duration(seconds: 2),
-      () =>
-          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
-    );
-
 Future<String> getUserId() async {
   // Call the fetchUserData() function to get user data.
   String userDataJson = await fetchUserData();

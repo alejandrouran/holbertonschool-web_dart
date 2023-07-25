55c8290012830c2348c9b50d8d3d36e5d94052dd
Alejandro Uran <alejandrourn@gmail.com>
2023-07-25 12:48:22 -0500

    four task asynchronous dart

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,48 +1,21 @@
+import '4-util.dart';
 import 'dart:convert';
-import 'dart:async';
-import 'dart:core';
 
-Future<String> fetchUserOrders(String id) async {
-  var orders = {
-    "7ee9a243-01ca-47c9-aa14-0149789764c3": ["pizza", "orange"]
-  };
+Future<double> calculateTotal() async {
   try {
-    return Future.delayed(
-        const Duration(seconds: 2), () => json.encode(orders[id]));
-  } catch (err) {
-    return "error caught : $err";
-  }
-}
-
-Future<String> fetchUserData() => Future.delayed(
-      const Duration(seconds: 2),
-      () =>
-          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
-    );
-
-Future<String> fetchProductPrice(product) async {
-  var products = {"pizza": 20.30, "orange": 10, "water": 5, "soda": 8.5};
-  try {
-    return Future.delayed(
-        const Duration(seconds: 2), () => json.encode(products[product]));
-  } catch (err) {
-    return "error caught : $err";
-  }
-}
-
-calculateTotal() async {
-    try {
-        double price = 0;
-	
-        final Map<String, dynamic> userData = json.decode(await fetchUserData());
-        final String data = userData['id'];
-        final List<dynamic> userOrder = json.decode(await fetchUserOrders(data));
-        for (int idx = 0; idx < userOrder.length; idx++) {
-            price += json.decode(await fetchProductPrice(userOrder[idx]));
+    return fetchUserData().then((user) {
+      String id = json.decode(user)["id"];
+      return fetchUserOrders(id).then((products) async {
+        double finalPrice = 0.0;
+        for (String product in json.decode(products)) {
+          await fetchProductPrice(product).then((price) {
+            finalPrice += json.decode(price);
+          });
         }
-        return price;
-    } catch (err) {
-        print('error caught: $err');
-        return -1;
-    }
+        return finalPrice;
+      });
+    });
+  } catch (err) {
+    return -1;
+  }
 }

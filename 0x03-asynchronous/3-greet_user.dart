b572170c2601c10ee0e0f116e180173d18028b09
Alejandro Uran <alejandrourn@gmail.com>
2023-07-25 11:44:22 -0500

    three task asynchronous

--- 0x03-asynchronous/3-greet_user.dart
+++ 0x03-asynchronous/3-greet_user.dart
@@ -1,18 +1,54 @@
-import 'dart:convert';
-import '4-util.dart';
+import 'dart:async';
 
-Future<num> calculateTotal() async {
+Future<String> fetchUserData() => Future.delayed(
+      const Duration(seconds: 2),
+      () =>
+          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
+    );
+
+Future<bool> checkCredentials() =>
+    Future.delayed(const Duration(seconds: 2), () => true);
+
+Future<String> greetUser() async {
   try {
-    Map<String, dynamic> user_data_response = json.decode(await fetchUserData());
-    String user_id = user_data_response['id'];
-    List<dynamic> user_orders_response = json.decode(await fetchUserOrders(user_id));
-    num total = 0;
-    for (String order in user_orders_response) {
-      total += json.decode(await fetchProductPrice(order));
+    // Call the fetchUserData() function to get user data.
+    String userDataJson = await fetchUserData();
+
+    // Parse the JSON data to a Map.
+    Map<String, dynamic> userData = json.decode(userDataJson);
+
+    // Extract the username from the user data.
+    String username = userData['username'];
+
+    // Return the greeting message.
+    return 'Hello $username';
+  } catch (error) {
+    // If an error occurs, catch it and return the error message.
+    return 'error caught: $error';
+  }
+}
+
+Future<String> loginUser() async {
+  try {
+    // Call the checkCredentials() function to check login credentials.
+    bool isUserLoggedIn = await checkCredentials();
+
+    // Print whether there is a user or not.
+    print('There is a user: $isUserLoggedIn');
+
+    if (isUserLoggedIn) {
+      // Call the greetUser() function to get the greeting message.
+      return await greetUser();
+    } else {
+      // Return the error message for wrong credentials.
+      return 'Wrong credentials';
     }
-    return total;
+  } catch (error) {
+    // If an error occurs, catch it and return the error message.
+    return 'error caught: $error';
   }
-  catch (error) {
-    return -1;
-  }
+}
+
+void main() async {
+  print(await loginUser());
 }

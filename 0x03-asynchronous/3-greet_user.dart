486a86e7d4455d7e85d301ba323f2c78f0179ef6
Alejandro Uran <alejandrourn@gmail.com>
2023-07-25 11:46:47 -0500

    three task asynchronous

--- 0x03-asynchronous/3-greet_user.dart
+++ 0x03-asynchronous/3-greet_user.dart
@@ -9,47 +9,23 @@ Future<String> fetchUserData() => Future.delayed(
 
 Future<bool> checkCredentials() =>
     Future.delayed(const Duration(seconds: 2), () => true);
-
-Future<String> greetUser() async {
+    
+greetUser() async {
   try {
-    // Call the fetchUserData() function to get user data.
-    String userDataJson = await fetchUserData();
-
-    // Parse the JSON data to a Map.
-    Map<String, dynamic> userData = json.decode(userDataJson);
-
-    // Extract the username from the user data.
-    String username = userData['username'];
-
-    // Return the greeting message.
-    return 'Hello $username';
-  } catch (error) {
-    // If an error occurs, catch it and return the error message.
-    return 'error caught: $error';
+  return 'hello ${json.decode(await fetchUserData())['username']}';
+  } catch (err) {
+    print('error caught: $err');
   }
 }
 
-Future<String> loginUser() async {
-  try {
-    // Call the checkCredentials() function to check login credentials.
-    bool isUserLoggedIn = await checkCredentials();
-
-    // Print whether there is a user or not.
-    print('There is a user: $isUserLoggedIn');
-
-    if (isUserLoggedIn) {
-      // Call the greetUser() function to get the greeting message.
-      return await greetUser();
-    } else {
-      // Return the error message for wrong credentials.
-      return 'Wrong credentials';
-    }
-  } catch (error) {
-    // If an error occurs, catch it and return the error message.
-    return 'error caught: $error';
+loginUser() async {
+ try {
+  if (await checkCredentials()) {
+    return await greetUser();
+  } else {
+    return 'Wrong credentials';
   }
-}
-
-void main() async {
-  print(await loginUser());
+} catch (err) {
+    print('error caught: $err');
+  }
 }

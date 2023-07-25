0e0c985eefffd4a0f5bd343bab009dba377ca754
Alejandro Uran <alejandrourn@gmail.com>
2023-07-25 11:33:49 -0500

    update two task asynchronous

--- 0x03-asynchronous/2-get_user.dart
+++ 0x03-asynchronous/2-get_user.dart
@@ -2,10 +2,9 @@ import '2-util.dart';
 
 Future<void> getUser() async {
   try {
-    // Call the fetchUser() function to get user data.
     print (await fetchUser());
-  } catch (error) {
-    // If an error occurs, catch it and print the error message.
-    print('Error caught: $error');
+  }
+  catch (error) {
+    print ('error caught: $error');
   }
 }
